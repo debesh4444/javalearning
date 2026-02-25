886ac0743c074d7d41c036678db23c543cb4cd86
debesh4444 <joynayak4444@gmail.com>
2026-02-26 00:02:29 +0530

    Add files via upload

---
+++ AC.java
@@ -0,0 +1,25 @@
+package Objects;
+
+public class AC {
+    int length;
+    int width;
+    String color;
+    String model;
+
+    ACcompressure compressure;
+    ACcondenser condenser;
+    ACExpansion_valve Expansion_valve;
+}
+class ACcompressure{
+    int size;
+    String material;
+
+}
+class ACcondenser{
+int size;
+String material;
+}
+class ACExpansion_valve {
+    int size;
+    String material;
+}
